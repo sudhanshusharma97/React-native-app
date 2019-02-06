f914b78432aeded89764151256173a88ee9500d9
sudhanshusharma97 <36498369+sudhanshusharma97@users.noreply.github.com>
2019-02-06 16:27:44 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,49 +1,104 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, TouchableHighlight, TouchableOpacity , Alert} from 'react-native';
+import { Button, View, Text, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 
+class HomeScreen extends React.Component {
+  static navigationOptions = {
 
-export default class App extends React.Component {
+     headerStyle: {
+       backgroundColor: '#000000',
+       shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 4,
+},
+shadowOpacity: 0.32,
+shadowRadius: 10.46,
 
-  _onPressButton() {
-    Alert.alert('App still in progress, please wait')
-  }
+elevation: 9,
 
+     },
+     headerTintColor: '#FFFFFF',
+     headerTitleStyle: {
+       fontWeight: 'bold',
+     },
+   };
 
   render() {
-
-
-
     return (
-      <View style={styles.container}>
-
-<View elevation={8} style={styles.header}></View>
-
+<View style={styles.container}>
 <Text style={styles.text}>Ohm's Law calculator</Text>
 <Image  style={styles.image} source={require('./ohm.png')} />
-<View style={[{ top:110, marginLeft: 'auto',
-marginRight: 'auto'}]}>
+        <TouchableOpacity elevation={20} onPress={() => this.props.navigation.navigate('Details')}
+          style={styles.submit} >
+        <Text style={styles.submitText}>Enter</Text>
+        </TouchableOpacity>
 
-
-<TouchableOpacity elevation={20} onPress={this._onPressButton}
-  style={styles.submit} >
-<Text style={styles.submitText}>Enter</Text>
-</TouchableOpacity>
-
-</View>
-
-
-</View>
+    </View>
 
 
     );
   }
 }
 
+class DetailsScreen extends React.Component {
+  static navigationOptions = {
+
+     headerStyle: {
+       backgroundColor: '#000000',
+       shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 4,
+},
+shadowOpacity: 0.32,
+shadowRadius: 10.46,
+
+elevation: 9,
+
+     },
+     headerTintColor: '#FFFFFF',
+     headerTitleStyle: {
+       shadowColor: '#FFFFFF',
+       fontWeight: 'bold',
+     },
+   };
+
+render() {
+    return (
+      <View style={styles.containercalculator}>
+        <Text style={styles.textcalculator} >Add values below</Text>
+        <TextInput style={styles.textinput}
+        {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+        editable = {true}
+        maxLength = {40}
+      />
+      <Text></Text>
+      <TextInput style={styles.textinput}
+      {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+      editable = {true}
+      maxLength = {40}
+    />
+        <View elevation={8} style={styles.bottomheader}></View>
+      </View>
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
 const styles = StyleSheet.create({
 
 
   submit:{
-    top:100,
+    top:140,
     width:150,
     marginRight:40,
     marginLeft:40,
@@ -77,8 +132,8 @@ alignSelf: 'stretch'
 
 image:{
 top: 100,
-height: 200,
-width: 200,
+height: 150,
+width: 150,
 marginLeft: 'auto',
 marginRight: 'auto'
 
@@ -95,6 +150,19 @@ fontFamily: 'notoserif'
 
 },
 
+textcalculator:{
+top: 60,
+  color: 'black',
+      fontWeight: 'bold',
+      fontSize: 32,
+      justifyContent: 'center',
+textAlign:'center',
+fontFamily: 'notoserif',
+
+alignSelf:'center',
+
+justifyContent:'center'
+},
 
 
 
@@ -106,25 +174,68 @@ container: {
 
   },
 
+  containercalculator: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: 'white',
+justifyContent:'center',
+
+    },
 
 
-header:{
-backgroundColor:'black',
+bottomheader:{
+backgroundColor:'#00B2EB',
 flexDirection:'row',
-flex:0.15,
-shadowOpacity: 0.75,
-        shadowRadius: 800,
-        shadowColor: 'black',
-        shadowOffset: { height: 100, width: 0 },
+flex:1,
+top:350,
 
+},
 
+textinput:{
 
-
-
-
-
+top:100,
+alignSelf:'center',
+justifyContent:'center',
+borderBottomColor: '#000000',
+borderWidth: 1,
+width:200,
+height:40,
+borderRadius: 50,
+textAlign: 'center',
 
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
